362d8360011b332b579c1a7c55a95ea6cfe23d5c
Darshil Shah <darshils99@gmail.com>
2026-01-04 19:48:11 -0600

    Update CoreFunctionalityTest.java

--- src/test/java/com/example/tests/CoreFunctionalityTest.java
+++ src/test/java/com/example/tests/CoreFunctionalityTest.java
@@ -1,5 +1,6 @@
 package com.example.tests;
 
+import com.example.framework.RetryAnalyzer;
 import com.example.pages.GoogleHomePage;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
@@ -7,27 +8,44 @@ import org.testng.annotations.Test;
 
 public class CoreFunctionalityTest extends BaseTest {
 
-  @Test
-  public void testSearchButtonPresence() {
-    driver.get("https://www.google.com");
-    GoogleHomePage homePage = new GoogleHomePage(driver);
-    Assert.assertTrue(homePage.isSearchButtonDisplayed(), "Search button is not displayed");
-  }
+    @Test(priority = 1, description = "Verify search button presence on Google homepage",
+          retryAnalyzer = RetryAnalyzer.class)
+    public void testSearchButtonPresence() {
+        logger.info("Starting testSearchButtonPresence");
+        navigateTo("https://www.google.com");
+        
+        GoogleHomePage homePage = new GoogleHomePage(driver);
+        boolean isDisplayed = homePage.isSearchButtonDisplayed();
+        
+        logger.info("Search button displayed: " + isDisplayed);
+        Assert.assertTrue(isDisplayed, "Search button is not displayed");
+        logger.info("testSearchButtonPresence passed");
+    }
 
-  @DataProvider(name = "searchQueries")
-  public Object[][] createSearchQueries() {
-    return new Object[][] {
-      {"TestNG"},
-      {"Selenium"},
-      {"Maven"}
-    };
-  }
+    @DataProvider(name = "searchQueries")
+    public Object[][] createSearchQueries() {
+        return new Object[][] {
+            {"TestNG"},
+            {"Selenium"},
+            {"Maven"}
+        };
+    }
 
-  @Test(dataProvider = "searchQueries")
-  public void testDataDrivenSearch(String query) {
-    driver.get("https://www.google.com");
-    GoogleHomePage homePage = new GoogleHomePage(driver);
-    homePage.searchFor(query);
-    Assert.assertTrue(driver.getTitle().contains(query), "Search result title does not contain query");
-  }
+    @Test(priority = 2, dataProvider = "searchQueries", 
+          description = "Data-driven search test",
+          retryAnalyzer = RetryAnalyzer.class)
+    public void testDataDrivenSearch(String query) {
+        logger.info("Starting testDataDrivenSearch with query: " + query);
+        navigateTo("https://www.google.com");
+        
+        GoogleHomePage homePage = new GoogleHomePage(driver);
+        homePage.searchFor(query);
+        
+        String pageTitle = driver.getTitle();
+        logger.info("Page title after search: " + pageTitle);
+        
+        Assert.assertTrue(pageTitle.contains(query), 
+                         "Search result title does not contain query: " + query);
+        logger.info("testDataDrivenSearch passed for query: " + query);
+    }
 }
