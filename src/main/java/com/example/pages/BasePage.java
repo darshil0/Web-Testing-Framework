1ff35aee9cdcc6a6436b2cc260f34adfc65f8faf
Darshil Shah <darshils99@gmail.com>
2026-01-04 19:36:36 -0600

    Create BasePage.java

---
+++ src/main/java/com/example/pages/BasePage.java
@@ -0,0 +1,33 @@
+package com.example.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BasePage {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
+
+    protected void clickElement(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+    }
+
+    protected void sendKeysToElement(WebElement element, String text) {
+        wait.until(ExpectedConditions.visibilityOf(element)).sendKeys(text);
+    }
+
+    protected String getElementText(WebElement element) {
+        return wait.until(ExpectedConditions.visibilityOf(element)).getText();
+    }
+}
