3453bb2dbd22d36a371a9e8fc478894b07cc5d08
Darshil Shah <darshils99@gmail.com>
2026-01-04 19:36:06 -0600

    Create ExtentTestManager.java

---
+++ src/main/java/com/example/framework/ExtentTestManager.java
@@ -0,0 +1,23 @@
+package com.example.framework;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
+public class ExtentTestManager {
+
+    private static ThreadLocal<ExtentTest> extentTest = new ThreadLocal<>();
+    private static ExtentReports extent = ExtentManager.getInstance();
+
+    public static synchronized ExtentTest getTest() {
+        return extentTest.get();
+    }
+
+    public static synchronized void startTest(String testName) {
+        ExtentTest test = extent.createTest(testName);
+        extentTest.set(test);
+    }
+
+    public static synchronized void endTest() {
+        extent.flush();
+    }
+}
