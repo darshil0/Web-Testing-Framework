04b43315260ab49e203db5f8d2557f56ae2374f0
Darshil Shah <darshils99@gmail.com>
2026-01-04 19:54:10 -0600

    Update WebDriverFactory.java

--- src/main/java/com/example/framework/WebDriverFactory.java
+++ src/main/java/com/example/framework/WebDriverFactory.java
@@ -5,30 +5,58 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class WebDriverFactory {
 
-  public static WebDriver createWebDriver(String browser) {
-    WebDriver driver;
-    switch (browser.toLowerCase()) {
-      case "chrome":
-        WebDriverManager.chromedriver().browserVersion("stable").setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--window-size=1920,1080");
-        driver = new ChromeDriver(options);
-        break;
-      case "firefox":
-        WebDriverManager.firefoxdriver().setup();
-        driver = new FirefoxDriver();
-        break;
-      default:
-        throw new IllegalArgumentException("Unsupported browser: " + browser);
+    private static final Logger logger = LogManager.getLogger(WebDriverFactory.class);
+
+    public static WebDriver createWebDriver(String browser) {
+        return createWebDriver(browser, false);
     }
-    driver.manage().window().maximize();
-    return driver;
-  }
+
+    public static WebDriver createWebDriver(String browser, boolean headless) {
+        WebDriver driver;
+        logger.info("Creating WebDriver for browser: " + browser + " (headless: " + headless + ")");
+
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                
+                if (headless) {
+                    chromeOptions.addArguments("--headless=new");
+                }
+                
+                chromeOptions.addArguments("--disable-gpu");
+                chromeOptions.addArguments("--no-sandbox");
+                chromeOptions.addArguments("--disable-dev-shm-usage");
+                chromeOptions.addArguments("--window-size=1920,1080");
+                chromeOptions.addArguments("--disable-blink-features=AutomationControlled");
+                
+                driver = new ChromeDriver(chromeOptions);
+                break;
+
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                
+                if (headless) {
+                    firefoxOptions.addArguments("--headless");
+                }
+                
+                driver = new FirefoxDriver(firefoxOptions);
+                break;
+
+            default:
+                logger.error("Unsupported browser: " + browser);
+                throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
+
+        driver.manage().window().maximize();
+        logger.info("WebDriver created successfully");
+        return driver;
+    }
 }
